5d16b0d453cfadacaf5d6c3f34ac55461105dc00
ehenrykang <ehenrykang@yahoo.com>
2022-04-06 17:31:20 -0700

    Added Selenium scripts

---
+++ aos_locators.py
@@ -0,0 +1,25 @@
+from faker import Faker
+fake = Faker(locale='en_CA')
+aos_url = 'https://advantageonlineshopping.com/#/'
+aos_title = 'Advantage Shopping'
+aos_homepage_title = '\xa0Advantage Shopping'
+aos_register_url = 'https://advantageonlineshopping.com/#/register'
+aos_login_url = 'https://advantageonlineshopping.com/#/'
+new_username = f'{fake.user_name()[:12]}{fake.pyint(111,999)}'
+# new_username = f'{fake.internet.userName()}'
+new_password = fake.password()[:12]
+new_firstname = fake.first_name()
+new_lastname = fake.last_name()
+email = fake.email()
+phone = fake.phone_number()
+city = fake.city()
+full_name = f'{new_firstname} {new_lastname}'
+address1 = f'{fake.street_address()}, {city}, {fake.province_abbr()}, {fake.current_country()} {fake.postalcode_in_province()}'
+address = fake.street_address()
+postal_code = fake.postalcode_in_province()
+state_province_region = fake.province_abbr()
+country = fake.current_country()
+aos_username = f'{fake.user_name()}{fake.pyint(111,999)}'
+# aos_username = f'{fake.internet.userName()}'
+aos_password = f'{fake.password()[:12]}'
+new_user_url = 'https://advantageonlineshopping.com/#/myAccount'
---
+++ aos_methods.py
@@ -0,0 +1,161 @@
+import datetime
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+import aos_locators as locators
+from selenium.webdriver.support.ui import Select
+# Using Selenium WebDriver, open the web browser.
+
+s = Service(executable_path='../chromedriver.exe')
+driver = webdriver.Chrome(service=s)
+
+
+# Fixture method - to open web browser
+def setUp():
+    print(f'Test started: {datetime.datetime.now()}')
+    # Make a full screen
+    driver.maximize_window()
+
+    # Wait for the browser response in general
+    driver.implicitly_wait(30)
+
+    # Navigating to the AOS web app URL - https://advantageonlineshopping.com/#/
+    driver.get(locators.aos_url)
+
+    # Checking that we're on the correct URL address and we're seeing correct title as expected.
+    if driver.current_url == locators.aos_url and driver.title == locators.aos_homepage_title:
+        print(f'Great! Advantage Online Shopping is launched! URL: {driver.current_url}')
+        print(f'We are seeing the page title: ', {driver.title})
+
+    else:
+        print(f'We are NOT at the Advantage Online Shopping Homepage! Please try again or check your code.')
+
+
+# Create New User Account - using Faker library fake data
+def create_new_user():
+    print(f'----------------------------------* CREATE NEW USER *----------------------------------')
+    driver.find_element(By.ID, 'menuUser').click()
+    sleep(2)
+    driver.find_element(By.LINK_TEXT, 'CREATE NEW ACCOUNT').click()
+    sleep(1)
+    if driver.current_url == locators.aos_register_url:
+        sleep(0.5)
+        # Enter Account Details
+        driver.find_element(By.NAME, 'usernameRegisterPage').send_keys(locators.new_username)
+        sleep(0.25)
+        driver.find_element(By.NAME, 'emailRegisterPage').send_keys(locators.email)
+        sleep(0.25)
+        driver.find_element(By.NAME, 'passwordRegisterPage').send_keys(locators.new_password)
+        sleep(0.25)
+        driver.find_element(By.NAME, 'confirm_passwordRegisterPage').send_keys(locators.new_password)
+        sleep(0.5)
+
+        # Enter Personal Details
+        driver.find_element(By.NAME, 'first_nameRegisterPage').send_keys(locators.new_firstname)
+        sleep(0.25)
+        driver.find_element(By.NAME, 'last_nameRegisterPage').send_keys(locators.new_lastname)
+        sleep(0.25)
+        driver.find_element(By.NAME, 'phone_numberRegisterPage').send_keys(locators.phone)
+        sleep(0.5)
+
+        # Enter Address information
+        Select(driver.find_element(By.NAME, 'countryListboxRegisterPage')).select_by_visible_text('Canada')
+        sleep(0.25)
+        driver.find_element(By.NAME, 'cityRegisterPage').send_keys(locators.city)
+        sleep(0.25)
+        driver.find_element(By.NAME, 'addressRegisterPage').send_keys(locators.address)
+        sleep(0.25)
+        driver.find_element(By.NAME, 'state_/_province_/_regionRegisterPage').send_keys(locators.state_province_region)
+        sleep(0.25)
+        driver.find_element(By.NAME, 'postal_codeRegisterPage').send_keys(locators.postal_code)
+        sleep(0.5)
+
+        # Checkbox for "I agree..."
+        driver.find_element(By.XPATH, '//*[@id="formCover"]/sec-view/div/input').click()
+        sleep(0.5)
+
+        # Create new account
+        driver.find_element(By.XPATH, '//*[@id="registerPage"]/article/sec-form/div[2]/sec-sender').click()
+        sleep(0.5)
+        print(f'--- Test Scenario: Create a new user with Username: {locators.new_username} and Password: {locators.new_password} --- is passed')
+
+    else:
+        print(f'We are not at the CREATE A NEW ACCOUNT page.')
+
+
+# Validate New Account created (new username is displayed in the top menu)
+def validate_new_user_created():
+    print(f'----------------------------------* VALIDATE NEW USER *----------------------------------')
+    if driver.current_url == locators.aos_url:
+        assert driver.find_element(By.XPATH, '//h3[text() = "SPECIAL OFFER"]').is_displayed()
+        sleep(0.5)
+        print(f'New User Account fullname is: {locators.full_name}')
+        print(f'New User Account address is: {locators.address1}')
+
+    else:
+        print(f'New User Account not created successfully. Please verify all the required fields (*) are completed.')
+
+
+# Logout
+def log_out():
+    print(f'----------------------------------* LOGOUT *----------------------------------')
+    driver.find_element(By.ID, 'menuUserLink').click()
+    sleep(1)
+    driver.find_element(By.XPATH, '//*[@id="loginMiniTitle"]/label[3]').click()
+    sleep(1)
+    if driver.current_url == locators.aos_url:
+        print(f'Logged out successfully at: {datetime.datetime.now()}')
+        sleep(1)
+    else:
+        print(f'Unable to log out. Something went wrong.')
+
+
+# Login with New Account
+def log_in():
+    print(f'----------------------------------* LOGIN NEW USER *----------------------------------')
+    if driver.current_url == locators.aos_url:
+        driver.find_element(By.ID, 'menuUser').click()
+        sleep(3)
+        driver.find_element(By.XPATH, '/html/body/login-modal/div/div/div[3]/sec-form/sec-view[1]/div/input').send_keys(locators.new_username)
+        sleep(0.5)
+        driver.find_element(By.XPATH, '/html/body/login-modal/div/div/div[3]/sec-form/sec-view[2]/div/input').send_keys(locators.new_password)
+        sleep(0.5)
+        driver.find_element(By.ID, 'sign_in_btnundefined').click()
+        print(f'We logged in successfully with Username: {locators.new_username} and Password: {locators.new_password}')
+        print(f'--- New User Account with username {locators.new_username} is displayed at top right. Test Passed ---')
+        sleep(2)
+
+    else:
+        print(f'Log in with new user not successful. Please verify your code or login credentials.')
+
+
+# Close the browser and display a user-friendly message.
+def tearDown():
+    print(f'----------------------------------* TEARDOWN *----------------------------------')
+    if driver is not None:
+        print(f'Test scenario completed at: {datetime.datetime.now()}')
+        driver.close()
+        driver.quit()
+
+    else:
+        print(f'Unable to close and quit. Something went wrong.')
+
+
+# Open the web browser
+# setUp()
+
+# Create New User
+# create_new_user()
+
+# Validate New User created
+# validate_new_user_created()
+
+# Logout
+# log_out()
+
+# Login
+# log_in(locators.aos_username, locators.aos_password)
+
+# Close the browser
+# tearDown()
